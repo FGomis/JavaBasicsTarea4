8c3b7db03b871bb3336b9ad024e9fd5670ee8d3a
FGomis <fkgomis@gmail.com>
2021-07-07 11:16:53 +0200

    Create JavaBasicsTarea4.java

---
+++ JavaBasicsTarea4.java
@@ -0,0 +1,14 @@
+
+public class JavaBasicsTarea4 {
+
+	public static void main(String[] args) {
+		
+		int N = 10;
+		
+		System.out.println("Valor inicial de N = "+N);
+		System.out.println("N + 77 = "+(N+=77));
+		System.out.println("N - 3 = "+(N-=3));
+		System.out.println("N * 2 = "+(N=N*2));
+	}
+
+}
